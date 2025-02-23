e44f3a4775cf71d7ed98d739381c854cefdbfe4b
kvqn <guneetaggarwal@gmail.com>
2025-02-23 21:11:39 +0530

    fix: linting

--- tools/container-manager/main.go
+++ tools/container-manager/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"container-manager/handlers/create"
 	"container-manager/handlers/exec"
-	"container-manager/handlers/is-running"
+	is_running "container-manager/handlers/is-running"
 	"container-manager/handlers/kill"
 	"fmt"
 	"net/http"
