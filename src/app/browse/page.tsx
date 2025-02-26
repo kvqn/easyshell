35009430151fbbb8cdc85b3282f3d31c4a68b852
kvqn <guneetaggarwal@gmail.com>
2025-02-26 18:09:30 +0530

    fix: linting

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -17,20 +17,18 @@ export default async function Page() {
   ).sort((a, b) => a.id - b.id)
 
   return (
-    <table className="w-full table-auto divide-y rounded-xl border">
-      <thead>
-        <tr className="divide-x *:p-2">
-          <th>#</th>
-          <th>Title</th>
-          <th>Solved</th>
-        </tr>
-      </thead>
-      <tbody className="divide-y">
+    <div className="w-full divide-y rounded-xl border overflow-hidden border-neutral-400">
+      <div className="divide-x divide-neutral-300 *:p-2 flex font-semibold bg-gray-200 border-b border-b-neutral-400">
+        <div className="w-20 text-center">#</div>
+        <div className="grow">Title</div>
+        <div className="w-20 lg:w-40">Solved</div>
+      </div>
+      <div className="divide-y">
         {problems.map((problem) => (
           <Problem key={problem.id} info={problem} />
         ))}
-      </tbody>
-    </table>
+      </div>
+    </div>
   )
 }
 
@@ -42,11 +40,11 @@ async function Problem({
   return (
     <Link
       href={`/problems/${info.slug}`}
-      className="table-row cursor-pointer divide-x transition-colors *:p-2 hover:bg-gray-100"
+      className="cursor-pointer divide-x transition-colors *:p-2 hover:bg-gray-100 flex"
     >
-      <td className="text-center">{info.id}</td>
-      <td>{info.slug}</td>
-      <td>No</td>
+      <div className="w-20 text-center">{info.id}</div>
+      <div className="grow">{info.slug}</div>
+      <div className="w-20 lg:w-40">No</div>
     </Link>
   )
 }
