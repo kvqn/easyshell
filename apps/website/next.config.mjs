db046797cdec875f6fad14c9192243c14caf798c
kvqn <guneetaggarwal@gmail.com>
2025-03-19 03:12:54 +0530

    chore: removed SKIP_ENV_VALIDATION from package.json scripts

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,10 +1,5 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev"
 import createMDX from "@next/mdx"
-import { join } from "path"
-
-// if (!process.env.SKIP_ENV_VALIDATION && process.env.APP !== "website")
-//   throw new Error("Invalid APP variable")
-//
 
 /** @type {import('next').NextConfig} */
 const config = {
@@ -13,8 +8,6 @@ const config = {
   experimental: {
     mdxRs: true,
   },
-  // outputFileTracingRoot: process.env.PWD,
-  // outputFileTracingRoot: join(process.env.PWD ?? "/app", "../../"),
 }
 
 const withMDX = createMDX({})
