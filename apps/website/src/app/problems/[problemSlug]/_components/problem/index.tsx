bab9b172d26719931bd9a083ed063ff2449c3b00
kvqn <guneetaggarwal@gmail.com>
2025-04-11 20:56:58 +0530

    style: keep problem heading fixed

--- apps/website/src/app/problems/[problemSlug]/_components/problem/index.tsx
+++ apps/website/src/app/problems/[problemSlug]/_components/problem/index.tsx
@@ -5,11 +5,13 @@ import { ProblemHints } from "./hints"
 
 export function Problem({ slug }: { slug: string }) {
   return (
-    <div className="overflow-y-scroll h-[calc(100vh-80px)] py-2">
+    <div className="h-[calc(100vh-80px)] py-2">
       <ProblemHeading slug={slug} />
-      <ProblemBody slug={slug} />
-      <ProblemHints slug={slug} />
-      <ProblemAbout slug={slug} />
+      <div className="overflow-y-scroll h-[calc(100vh-220px)]">
+        <ProblemBody slug={slug} />
+        <ProblemHints slug={slug} />
+        <ProblemAbout slug={slug} />
+      </div>
     </div>
   )
 }
