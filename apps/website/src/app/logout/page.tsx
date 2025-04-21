909351bf4fd96debfac7b2dfb9e766704176c5f8
kvqn <guneetaggarwal@gmail.com>
2025-04-21 19:05:52 +0530

    style: adjusted auth pages and added email login (not implemented)

--- apps/website/src/app/logout/page.tsx
+++ apps/website/src/app/logout/page.tsx
@@ -1,24 +1,31 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
 
 import { signOut } from "next-auth/react"
+import Link from "next/link"
 
 export default function Page() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="flex flex-col gap-4 rounded-xl border p-8">
-        <h2 className="text-center font-semibold">Are you sure?</h2>
-        <Button
-          className="flex items-center gap-2"
-          variant={"destructive"}
-          onClick={async () => {
-            await signOut()
-          }}
-        >
-          Log Out
-        </Button>
-      </div>
+      <Card className="flex w-fit flex-col rounded-xl border px-8 py-4">
+        <h2 className="text-lg font-semibold">{`You're about to log out`}</h2>
+        <h3 className="text-sm text-neutral-400">{`Are you sure you want to log out?`}</h3>
+        <div className="mt-4 flex gap-2 *:*:w-full *:grow">
+          <Link href="/">
+            <Button variant={"secondary"}>Cancel</Button>
+          </Link>
+          <Button
+            variant={"destructive"}
+            onClick={async () => {
+              await signOut()
+            }}
+          >
+            Log Out
+          </Button>
+        </div>
+      </Card>
     </div>
   )
 }
