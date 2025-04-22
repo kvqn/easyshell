1286b48bfc283bf4e4f1a6d7cd716d4d95d90c05
kvqn <guneetaggarwal@gmail.com>
2025-04-22 16:36:27 +0530

    fix: resend api env var
    idk why this is even needed

--- apps/website/src/lib/server/resend.ts
+++ apps/website/src/lib/server/resend.ts
@@ -1,5 +1,3 @@
-import { env } from "@easyshell/env"
-
 import { Resend } from "resend"
 
-export const resend = new Resend(env.RESEND_API_KEY)
+export const resend = new Resend(process.env.RESEND_API_KEY)
