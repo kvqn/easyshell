a4332982797b5ef6957a0a4496d0495f28b2a5e5
kvqn <guneetaggarwal@gmail.com>
2025-05-07 15:02:11 +0530

    chore: problem link component

--- apps/website/src/app/wiki/[slug]/page.tsx
+++ apps/website/src/app/wiki/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { Footer } from "@/app/_components/footer"
+import { ProblemLink } from "@/components/problem-link"
 import { ProblemStatus } from "@/components/problem-status"
 import { auth } from "@/lib/server/auth"
 import { getProblemDifficulty, getProblemStatus } from "@/lib/server/problems"
@@ -83,23 +84,7 @@ export async function SpoilerWarning({
       <span className="font-clash-display text-neutral-700 dark:text-neutral-300">
         {`This wiki page contains spoilers for the problem `}
       </span>
-      <Link
-        href={`/problems/${slug}`}
-        className={cn("ml-1 w-fit rounded-md border px-2 py-1 shadow-xs", {
-          "bg-green-100": difficulty === "easy",
-          "border-orange-400 bg-orange-100 shadow-orange-400 dark:border-orange-600 dark:bg-orange-900 dark:shadow-orange-600":
-            difficulty === "medium",
-        })}
-      >
-        <span
-          className={cn("font-geist-mono text-xs font-medium", {
-            "text-orange-600 dark:text-orange-400": difficulty === "medium",
-          })}
-        >
-          {slug}
-        </span>
-        <ProblemStatus status={status} />
-      </Link>
+      <ProblemLink slug={slug} status={status} difficulty={difficulty} />
     </div>
   )
 }
---
+++ apps/website/src/components/problem-link.tsx
@@ -0,0 +1,52 @@
+import { cn } from "@/lib/utils"
+
+import { ProblemStatus } from "./problem-status"
+import { EasyTooltip } from "./ui/tooltip"
+
+import Link from "next/link"
+
+export async function ProblemLink({
+  slug,
+  status,
+  difficulty,
+  className,
+}: {
+  slug: string
+  status?: "attempted" | "solved"
+  difficulty: "easy" | "medium" | "hard"
+  className?: string
+}) {
+  return (
+    <EasyTooltip
+      tip={
+        status === "solved"
+          ? "Solved"
+          : status === "attempted"
+            ? "Attempted"
+            : "Not Attempted"
+      }
+    >
+      <Link
+        href={`/problems/${slug}`}
+        className={cn(
+          "ml-1 w-fit rounded-md border px-2 py-1 shadow-xs",
+          {
+            "bg-green-100": difficulty === "easy",
+            "border-orange-400 bg-orange-100 shadow-orange-400 dark:border-orange-600 dark:bg-orange-900 dark:shadow-orange-600":
+              difficulty === "medium",
+          },
+          className,
+        )}
+      >
+        <span
+          className={cn("font-geist-mono text-xs font-medium", {
+            "text-orange-600 dark:text-orange-400": difficulty === "medium",
+          })}
+        >
+          {slug}
+        </span>
+        <ProblemStatus status={status} />
+      </Link>
+    </EasyTooltip>
+  )
+}
